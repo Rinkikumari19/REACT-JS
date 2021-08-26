2d7c26cdebf0439e3fdf0c948e60a22adc62c1ee
priyankang <priyankak18@navgurukul.org>
2021-08-26 08:58:07 +0530

    use weather api

---
+++ My_Friend/my-friends/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Friends from './components/Friends';
+function App() {
+  return (
+    <div className="App">
+      <Friends img='https://i.pinimg.com/originals/f6/74/a1/f674a123a6046545e68f8d55901d8a71.png'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ My_Friend/my-friends/src/components/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Friends(props){
+    return<div className='main'>
+        <h1 style={{fontSize:'xxx-large'}}> Happy Friendship Day 😍</h1>
+        <img src={props.img} style ={{width : '40%'}}/>
+
+        <p className='para'><h1>About Friendship</h1>Friendship is a very important part in our life, Nothing matter that, where they are live from far you or from near you but we trust a lot on our friendship because they are only that person with whom we share our feelings, If our friendship is strong so, No one can be break and we can do everything, nothing matter that this relastionship should be bood relationship or not just we should keep trust always the automatically it will be become more powerfull :)  </p>
+        <p className='para1'><h1>My Thoughts</h1>I want to say thankyou to my dear friends who are in my life, who always helped me in my bad situation, Who care me, who understand me, who guide me, who motivate me always. I love you so much my dear friends. My wish is this only that never break our freindship. Our relationship will live always. </p>
+        </div>
+}
+export default Friends;
